b7f8b84e674bd65f042a8927e9d8d3ebaa853975
lnyan <lkwq007@gmail.com>
2020-12-12 11:28:27 +0800

    Update env

--- plan2explore/env.py
+++ plan2explore/env.py
@@ -4,7 +4,7 @@ import torch
 
 from env_utils import make_envs
 
-GYM_ENVS = ["TetrisA-v0","TetrisA-v1",'Pendulum-v0', 'MountainCarContinuous-v0', 'Ant-v2', 'HalfCheetah-v2', 'Hopper-v2', 'Humanoid-v2', 'HumanoidStandup-v2', 'InvertedDoublePendulum-v2', 'InvertedPendulum-v2', 'Reacher-v2', 'Swimmer-v2', 'Walker2d-v2']
+GYM_ENVS = ["Tetris-v0","TetrisA-v0","TetrisA-v1",'Pendulum-v0', 'MountainCarContinuous-v0', 'Ant-v2', 'HalfCheetah-v2', 'Hopper-v2', 'Humanoid-v2', 'HumanoidStandup-v2', 'InvertedDoublePendulum-v2', 'InvertedPendulum-v2', 'Reacher-v2', 'Swimmer-v2', 'Walker2d-v2']
 CONTROL_SUITE_ENVS = ['cartpole-balance', 'cartpole-swingup', 'reacher-easy', 'finger-spin', 'cheetah-run', 'ball_in_cup-catch', 'walker-walk','reacher-hard', 'walker-run', 'humanoid-stand', 'humanoid-walk', 'fish-swim', 'acrobot-swingup']
 CONTROL_SUITE_ACTION_REPEATS = {'cartpole': 8, 'reacher': 4, 'finger': 2, 'cheetah': 4, 'ball_in_cup': 6, 'walker': 2, 'humanoid': 2, 'fish': 2, 'acrobot':4}
 
@@ -89,6 +89,7 @@ class ControlSuiteEnv():
 import gym
 from nes_py.wrappers import JoypadSpace
 import gym_tetris
+import gym_tetris_simple
 from gym_tetris.actions import MOVEMENT,SIMPLE_MOVEMENT
 from gym import spaces
 import cv2
@@ -104,7 +105,48 @@ def crop_image(image):
     image = cv2.resize(image, (10,20))
     image = image.astype(np.float32)
     return image
+
+def crop_image_simple(image):
+    '''
+    Args: image as an numpy array
+    returns: a crop image as a standardised numpy array
+    '''
+    image = np.mean(image, axis=2)
+    image[image > 0] = 1
+    image = cv2.resize(image, (10,20))
+    image = image.astype(np.float32)
+    return image
+
+class SymbolTetrisSimple(gym.Wrapper):
+  def __init__(self, env):
+    gym.Wrapper.__init__(self, env)
+    shp = env.observation_space.shape
+    # set 
+    self.observation_space = spaces.Box(
+      low=0.0,
+      high=1.0,
+      shape=(200,),
+      dtype=np.float32
+    )
+
+  def reset(self):
+    obs = self.env.reset()
+    return self._get_board(obs)
   
+  def _get_board(self,obs):
+    return crop_image_simple(obs).reshape(-1)
+  
+  def _get_board_ram(self):
+    board=np.array(self.env.game_state.board)
+    board[board!="."]=1
+    board[board=="."]=0
+    board=board.astype(np.float32).transpose(1,0)
+    return board
+
+  def step(self, action):            
+    obs, reward, done, info = self.env.step(action)
+    return self._get_board(obs), reward, done, info
+
 class SymbolTetris(gym.Wrapper):
   def __init__(self, env):
     gym.Wrapper.__init__(self, env)
@@ -141,12 +183,107 @@ def get_bumpiness_height_hole(board):
   invert_heights = np.where(mask.any(axis=0), np.argmax(mask, axis=0), 20)
   heights = 20 - invert_heights
   total_height = np.sum(heights)
+  max_height = heights.max()
   currs = heights[:-1]
   nexts = heights[1:]
   diffs = np.abs(currs - nexts)
   total_bumpiness = np.sum(diffs)
   total_cell=np.sum(board)
-  return total_bumpiness, total_height, total_height-total_cell
+  return total_bumpiness, total_height, total_height-total_cell, max_height
+
+
+class GameEnv():
+  def __init__(self, env, symbolic, seed, max_episode_length, action_repeat, bit_depth, args):
+    symbolic=True
+    self.symbolic = symbolic
+    # self._env = gym.make(env)
+    self._env = gym_tetris.make(env)
+    self._env.seed(seed)
+    if symbolic:
+      self._env=SymbolTetrisSimple(self._env)
+    self.max_episode_length = max_episode_length
+    self.action_repeat = 1
+    self.bit_depth = bit_depth
+    self.typeb = "1" in env
+    self.acc = 0.01 if self.typeb else 3
+    self.living = 0.01 if self.typeb else 0.3
+    self.die=-50
+    self.score=0.0
+    self.add_reward=args.add_reward
+    if not args.add_reward:
+      self.acc=0
+      self.living=0
+      self.die=0
+
+  def reset(self):
+    self.t = 0  # Reset internal timer
+    state = self._env.reset()
+    self.score=0.0
+    if self.symbolic:
+      return torch.tensor(state, dtype=torch.float32).unsqueeze(dim=0)
+    else:
+      return _images_to_observation(state[46:-30,92:-12,:], self.bit_depth)
+
+  def _get_board_ram(self):
+    board=np.array(self._env.game_state.board)
+    board[board!="."]=1
+    board[board=="."]=0
+    board=board.astype(np.float32).transpose(1,0)
+    return board
+
+  def step(self, action):
+    # action = action.detach().numpy()
+    action = action.argmax().item()
+    reward = 0
+    state, done = None, None
+    last_board=self._get_board_ram()
+    for k in range(1):
+      state, reward_k, done, _ = self._env.step(action)
+      reward += reward_k
+      self.t += 1  # Increment internal timer
+      done = done or self.t == self.max_episode_length
+      if done:
+        break
+    if reward>0:
+      board=last_board
+    else:
+      board=self._get_board_ram()
+    if (reward>0) and self.add_reward:
+      bumpiness, heights, holes, max_heights=get_bumpiness_height_hole(board)
+      score = 6.76*reward - 0.51*heights - 0.36*holes - 0.28*bumpiness - 0.5*max_heights
+      reward = score-self.score
+      self.score=score
+      # reward+=self.acc
+      # if info['board_height']>10:
+      #   reward-=self.acc
+    reward+=self.living if action==0 else self.living/2
+    if done:
+      reward+=self.die
+    if self.symbolic:
+      observation = torch.tensor(state, dtype=torch.float32).unsqueeze(dim=0)
+    else:
+      observation = _images_to_observation(state[46:-30,92:-12,:], self.bit_depth)
+    return observation, reward, done
+
+  def render(self):
+    self._env.render()
+
+  def close(self):
+    self._env.close()
+
+  @property
+  def observation_size(self):
+    return self._env.observation_space.shape[0] if self.symbolic else (3, 96, 96)
+
+  @property
+  def action_size(self):
+    return self._env.action_space.n
+
+  # Sample an action randomly from a uniform distribution over all valid actions
+  def sample_random_action(self):
+    indices = torch.tensor(self._env.action_space.sample())
+    return F.one_hot(indices, self.action_size).float()
+    # return torch.tensor(self._env.action_space.sample())
 
 from torch.nn import functional as F
 class GymEnv():
@@ -163,8 +300,8 @@ class GymEnv():
     self.bit_depth = bit_depth
     self.typeb = "1" in env
     self.acc = 0.01 if self.typeb else 3
-    self.living = 0.001 if self.typeb else 0.3
-    self.die=-10
+    self.living = 0.01 if self.typeb else 0.3
+    self.die=-50
     self.score=0.0
     self.add_reward=args.add_reward
     if not args.add_reward:
@@ -215,8 +352,8 @@ class GymEnv():
       reward+=r
       done=d or done
     if (flag or reward>0) and self.add_reward:
-      bumpiness, heights, holes=get_bumpiness_height_hole(board)
-      score = 0.76*reward - 0.51*heights - 0.36*holes - 0.18*bumpiness
+      bumpiness, heights, holes, max_heights=get_bumpiness_height_hole(board)
+      score = 6.76*reward - 0.51*heights - 0.36*holes - 0.28*bumpiness - 0.5*max_heights
       reward = score-self.score
       self.score=score
       # reward+=self.acc
@@ -253,6 +390,8 @@ class GymEnv():
 
 
 def Env(env, symbolic, seed, max_episode_length, action_repeat, bit_depth, args):
+  if env=="Tetris_v0":
+    return GameEnv(env, symbolic, seed, max_episode_length, action_repeat, bit_depth, args) 
   if env in GYM_ENVS:
     return GymEnv(env, symbolic, seed, max_episode_length, action_repeat, bit_depth, args)
   elif env in CONTROL_SUITE_ENVS:
